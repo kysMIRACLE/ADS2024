8a6a5da880f00d2f1017e0eddb11344a3a62f5d0
ZenthonMiracle <156891752+kysMIRACLE@users.noreply.github.com>
2024-10-27 17:28:05 +0500

    Delete A.py

--- A.py
+++
@@ -1,9 +0,0 @@
-T = int(input().strip())
-
-for _ in range(T):
-    
-    strings = input().strip().split()
-    
-   
-    sorted_strings = sorted(strings, key=len)
-    print(" ".join(sorted_strings))
